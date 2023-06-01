a396306981e0c35fd0b13513e9b694973b5c9ddc
Bohdan <bohdanvoroniak455@gmail.com>
2023-06-02 00:20:38 +0300

    add error message object

---
+++ BLL/Dto/Errors/ErrorResponceMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Dto.Errors
+{
+    public class ErrorResponceMessage
+    {
+        [DataMember(Name ="error")]
+        public string Error { get; set; } = string.Empty;
+        [DataMember(Name = "message")]
+        public string Message { get; set; } = string.Empty;
+        [DataMember(Name = "details")]
+        public string Details { get; set; } = string.Empty;
+    }
+}
